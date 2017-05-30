f2ecbfd8c555a26d4e7d223327273f8279815073
Hoh Yu Onn <heyuan95@hotmail.com>
2017-05-31 04:03:53 +0800

    Control and Camera fixed

--- Assets/Hoh'Stuff/Scripts/PlayerController.cs
+++ Assets/Hoh'Stuff/Scripts/PlayerController.cs
@@ -1,48 +1,30 @@
 ﻿using UnityEngine;
 
-[RequireComponent(typeof(PlayerMotor))]
 public class PlayerController : MonoBehaviour {
-
+	
 	[SerializeField]
-	private float playerMoveSpeed = 5.0f;
+	private float playerMoveSpeed = 10.0f;
 	[SerializeField]
-	private float lookSensitivity = 5.0f;
-
-	private PlayerMotor motor;
+	private float playerRotateSpeed = 10.0f;
+	[SerializeField]
+	private Rigidbody rb;
 
 	// Use this for initialization
-	void Start ()
+	void Start () 
 	{
-		motor = GetComponent<PlayerMotor> (); 
+		rb = GetComponent<Rigidbody> ();
 	}
+
 	// Update is called once per frame
 	void Update () 
 	{
-		//Calculate movement velocity as a 3D vector
-		float _xMov = Input.GetAxis ("Horizontal");
-		float _zMov = Input.GetAxis ("Vertical");
-
-		Vector3 _moveHorizontal = transform.right * -_xMov;
-		Vector3 _moveVertical = transform.forward * -_zMov;
-
-		// Final movement vector
-		Vector3 _velocity = (_moveHorizontal + _moveVertical).normalized * playerMoveSpeed;
-
-		// Apply movement
-		motor.Move (_velocity);
-
-		// Calculate rotation as a 3D vector (turning around)
-		float _yRot = Input.GetAxis ("Mouse X");
-		float _rotationY = _yRot * lookSensitivity;
-
-		// Apply rotation
-		motor.Rotate (_rotationY);
-
-		// Calculate camera rotation as a 3D vector (turning around)
-		float _xRot = Input.GetAxis ("Mouse Y");
-		float _cameraRotationX = _xRot * lookSensitivity;
-
-		// Apply camera rotation
-		motor.RotateCamera (_cameraRotationX);
+		if (Input.GetKey (GameManager.GM.forward))
+			transform.Translate(Vector3.forward * playerMoveSpeed * Time.deltaTime);
+		else if (Input.GetKey (GameManager.GM.backward))
+			transform.Translate(Vector3.back * playerMoveSpeed * Time.deltaTime);
+		else if (Input.GetKey (GameManager.GM.left)) 
+			transform.Rotate (0,-playerRotateSpeed * Time.deltaTime,0);
+		else if(Input.GetKey(GameManager.GM.right))
+			transform.Rotate (0,playerRotateSpeed * Time.deltaTime,0);
 	}
 }
---
+++ Assets/Hoh'Stuff/Scripts/ThirdPersonCamera.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThirdPersonCamera : MonoBehaviour {
+
+	[SerializeField]
+	private Camera cam;
+	[SerializeField]
+	private bool lockCursor;
+	[SerializeField]
+	private float mouseSensitivity = 5;
+	[SerializeField]
+	private Vector2 yawMinMax = new Vector2 (-45, 45);
+	[SerializeField]
+	private Vector2 pitchMinMax = new Vector2 (-30, 30);
+	[SerializeField]
+	private float rotationSmoothTime = .12f;
+
+	Vector3 rotationSmoothVelocity;
+	Vector3 currentRotation;
+
+	float yaw;
+	float pitch;
+
+	void Start() {
+		if (lockCursor) {
+			Cursor.lockState = CursorLockMode.Locked;
+			Cursor.visible = false;
+		}
+	}
+
+	void LateUpdate () {
+		if (cam != null)
+		{
+			yaw += Input.GetAxis ("Mouse X") * mouseSensitivity;
+			yaw = Mathf.Clamp (yaw, yawMinMax.x, yawMinMax.y);
+			pitch -= Input.GetAxis ("Mouse Y") * mouseSensitivity;
+			pitch = Mathf.Clamp (pitch, pitchMinMax.x, pitchMinMax.y);
+
+			currentRotation = Vector3.SmoothDamp (currentRotation, new Vector3 (pitch, yaw), ref rotationSmoothVelocity, rotationSmoothTime);
+			cam.transform.localEulerAngles = currentRotation;
+		}
+	}
+
+}
