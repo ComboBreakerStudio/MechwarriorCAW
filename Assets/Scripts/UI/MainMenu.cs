7c057e2f9d19aa7c7b87036d822d9514edf07876
Richard Lee Guan Rong <guanrong1226@gmail.com>
2017-05-31 21:53:11 +0800

    .gitignore implemented and implementation of Main Menu and Settings along with keybind configuration, currently still bind to GameManager so it will be moved to its own manager in future builds

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	public void startGame ()
+	{
+		SceneManager.LoadScene ("Lobby");
+	}
+
+	public void Settings ()
+	{
+		SceneManager.LoadScene ("Settings");
+	}
+
+	public void exitGame ()
+	{
+		Application.Quit ();
+	}
+}
