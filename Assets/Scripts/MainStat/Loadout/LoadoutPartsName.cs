0e2abacdb8f8c81f7e6bd967d9ad44287d24cc01
William <felinxrynn@gmail.com>
2017-06-14 23:16:41 +0800

    Loadout System (no ui yet)

---
+++ Assets/Scripts/MainStat/Loadout/LoadoutPartsName.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadoutPartsName : MonoBehaviour {
+	
+	public string torsoName, leftWeaponSystemName, rightWeaponSystemName, legName;
+
+}
