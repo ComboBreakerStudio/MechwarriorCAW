44b03400d518e565086f313c4d411bb9c3a9bb9b
Richard Lee Guan Rong <guanrong1226@gmail.com>
2017-06-15 12:23:57 +0800

    UI Changes (#3)
    * test
    
    * minimap modification
    
    * more ui modification and minimal stat implementation
    
    * Modification to the Player UI and minimal integration of stat feedback

--- Assets/Scripts/UI/PlayerUI.cs
+++ Assets/Scripts/UI/PlayerUI.cs
@@ -1,15 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
 
 public class PlayerUI : MonoBehaviour {
 
 	[SerializeField]
 	private GameObject pauseMenu;
 
+	[SerializeField]
+	private GameObject minimap;
+
+	[SerializeField]
+	private Image heatBar;
+
+	[SerializeField]
+	private GameObject Health;
+
+	bool isLocalPlayer;
+
+	private GameObject HeadHealth;
+	private GameObject TorsoHealth;
+	private GameObject LeftTorsoHealth;
+	private GameObject LeftArmHealth;
+	private GameObject LeftLegHealth;
+	private GameObject RightTorsoHealth;
+	private GameObject RightArmHealth;
+	private GameObject RightLegHealth;
+
 	// Use this for initialization
 	void Start () {
 		PauseMenu.IsOn = false;
+		isLocalPlayer = true;
+		//heatBar = 
+		Debug.Log ("Found Left Ammo?");
+		//rightAmmo = GameObject.Find ("RightAmmo").GetComponentInChildren<Text> ();
+		//heatBar.fillAmount = GetComponent<WeaponSystemStats> ().currentHeat;
+		heatBar.fillAmount = 0;
+		foreach (Transform img in Health.transform) {
+			img.gameObject.SetActive (false);
+		}
 	}
 	
 	// Update is called once per frame
@@ -23,5 +54,13 @@ public class PlayerUI : MonoBehaviour {
 //		Debug.Log ("Pause");
 		pauseMenu.SetActive (!pauseMenu.activeSelf);
 		PauseMenu.IsOn = pauseMenu.activeSelf;
+	}
+
+	//[Client]
+	void mapDisplay ()
+	{
+		if (isLocalPlayer == true) {
+			minimap.SetActive (true);
+		}
 	}
 }
