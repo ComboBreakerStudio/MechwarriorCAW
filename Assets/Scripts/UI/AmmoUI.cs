3f48a3e7ab92a7cf78271eafd60512010c513993
CORSAIR240\Richard Wanker <guanrong1226@gmail.com>
2017-07-06 06:00:22 +0800

    UI Updates #2

--- Assets/Scripts/UI/AmmoUI.cs
+++ Assets/Scripts/UI/AmmoUI.cs
@@ -2,19 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Networking;
 
-public class AmmoUI : MonoBehaviour {
+
+public class AmmoUI : NetworkBehaviour {
+//public class AmmoUI : MonoBehaviour {
 
 	[SerializeField]
-	private GameObject leftAmmo;
+	private Text leftAmmo;
 
 	[SerializeField]
-	private GameObject rightAmmo;
+	private Text rightAmmo;
+
+	//[SyncVar]
+	public WeaponSystemStats leftWeaponAmmo, rightWeaponAmmo;
+
+	public PlayerStats playerStats;
 
 	// Use this for initialization
 	void Start () {
-		leftAmmo.GetComponentInChildren<Text>().text = "9";
-		rightAmmo.SetActive (false);
+		playerStats.StartStuff ();
+		Debug.Log ("Weapon system data acquired");
+		rightAmmo.gameObject.SetActive (false);
+		Debug.Log ("right side disabled");
+		leftAmmo.text = playerStats.leftWeaponSystemStats.currentAmmo.ToString ();
+		Debug.Log ("ammo data acquired");
 	}
 	
 	// Update is called once per frame
--- Assets/Scripts/UI/PlayerUI.cs
+++ Assets/Scripts/UI/PlayerUI.cs
@@ -18,7 +18,7 @@ public class PlayerUI : MonoBehaviour {
 	[SerializeField]
 	private GameObject Health;
 
-	bool isLocalPlayer;
+	//bool isLocalPlayer;
 
 	public Transform HeadHealth;
 	public Transform TorsoHealth;
@@ -29,14 +29,7 @@ public class PlayerUI : MonoBehaviour {
 	public Transform RightArmHealth;
 	public Transform RightLegHealth;
 
-	/*public GameObject HeadHealth;
-	public GameObject TorsoHealth;
-	public GameObject LeftTorsoHealth;
-	public GameObject LeftArmHealth;
-	public GameObject LeftLegHealth;
-	public GameObject RightTorsoHealth;
-	public GameObject RightArmHealth;
-	public GameObject RightLegHealth;*/
+	public Text ammoCount;
 
 	public PlayerStats playerStatScript;
 
@@ -48,9 +41,9 @@ public class PlayerUI : MonoBehaviour {
 	void Start () {
 		canShakeScreen = true;
 		PauseMenu.IsOn = false;
-		isLocalPlayer = true;
+		//isLocalPlayer = true;
 		//heatBar = 
-		Debug.Log ("Found Left Ammo?");
+		//Debug.Log ("Found Left Ammo?");
 		//rightAmmo = GameObject.Find ("RightAmmo").GetComponentInChildren<Text> ();
 		//heatBar.fillAmount = GetComponent<WeaponSystemStats> ().currentHeat;
 		heatBar.fillAmount = 0;
@@ -96,58 +89,8 @@ public class PlayerUI : MonoBehaviour {
 		if(Input.GetKeyDown(KeyCode.Escape)){
 			TogglePauseMenu ();
 		}
+		healthDisplay ();
 
-		if(playerStatScript != null){
-			//Front
-			if (playerStatScript.frontTorso_Health <= 0) {
-				TorsoHealth.gameObject.SetActive (false);
-			} 
-			else {
-				TorsoHealth.gameObject.SetActive (true);
-			}
-			//left torso
-			if (playerStatScript.leftTorso_Health <= 0) {
-				LeftTorsoHealth.gameObject.SetActive (false);
-			} 
-			else {
-				LeftTorsoHealth.gameObject.SetActive (true);
-			}
-			//Right Torso
-			if (playerStatScript.rightTorso_Health <= 0) {
-				RightTorsoHealth.gameObject.SetActive (false);
-			} 
-			else {
-				RightTorsoHealth.gameObject.SetActive (true);
-			}
-			//Left Weapon
-			if (playerStatScript.leftWeaponSystem_Health <= 0) {
-				LeftArmHealth.gameObject.SetActive (false);
-			} 
-			else {
-				LeftArmHealth.gameObject.SetActive (true);
-			}
-			//Right Weapon
-			if (playerStatScript.rightWeaponSystem_Health <= 0) {
-				RightArmHealth.gameObject.SetActive (false);
-			} 
-			else {
-				RightArmHealth.gameObject.SetActive (true);
-			}
-			//Left Leg
-			if (playerStatScript.leftLeg_Health <= 0) {
-				LeftLegHealth.gameObject.SetActive (false);
-			} 
-			else {
-				LeftLegHealth.gameObject.SetActive (true);
-			}
-			//Right Leg
-			if (playerStatScript.rightTorso_Health <= 0) {
-				RightLegHealth.gameObject.SetActive (false);
-			} 
-			else {
-				RightLegHealth.gameObject.SetActive (true);
-			}
-		}
 	}
 
 	private void TogglePauseMenu(){
@@ -190,6 +133,103 @@ public class PlayerUI : MonoBehaviour {
 	}
 	void healthDisplay()
 	{
-		
+		if(playerStatScript != null){
+			//Front
+			if (playerStatScript.frontTorso_Health <= 0) {
+				TorsoHealth.Find("BodyRed").gameObject.SetActive (false);
+			} 
+			else if(playerStatScript.frontTorso_Health <= 35) {
+				TorsoHealth.Find ("BodyGreen").gameObject.SetActive (false);
+				TorsoHealth.Find("BodyOrange").gameObject.SetActive (true);
+			}
+			else if(playerStatScript.frontTorso_Health <= 20) {
+				TorsoHealth.Find ("BodyOrange").gameObject.SetActive (false);
+				TorsoHealth.Find("BodyRed").gameObject.SetActive (true);
+			}
+			//Head
+			if (playerStatScript.backTorso_Health <= 0) {
+				HeadHealth.Find ("HeadRed").gameObject.SetActive (false);
+			} 
+			else if(playerStatScript.backTorso_Health <= 15) {
+				HeadHealth.Find ("HeadGreen").gameObject.SetActive (false);
+				HeadHealth.Find("HeadOrange").gameObject.SetActive (true);
+			}
+			else if(playerStatScript.backTorso_Health <= 10) {
+				HeadHealth.Find ("HeadOrange").gameObject.SetActive (false);
+				HeadHealth.Find("HeadRed").gameObject.SetActive (true);
+			}
+			//left torso
+			if (playerStatScript.leftTorso_Health <= 0) {
+				LeftTorsoHealth.Find ("LeftTorsoRed").gameObject.SetActive (false);
+			} 
+			else if (playerStatScript.leftTorso_Health <= 30) {
+				LeftTorsoHealth.Find ("LeftTorsoGreen").gameObject.SetActive (false);
+				LeftTorsoHealth.Find ("LeftTorsoOrange").gameObject.SetActive (true);
+			}
+			else if (playerStatScript.leftTorso_Health <= 15) {
+				LeftTorsoHealth.Find ("LeftTorsoOrange").gameObject.SetActive (false);
+				LeftTorsoHealth.Find ("LeftTorsoRed").gameObject.SetActive (true);
+			}
+			//Right Torso
+			if (playerStatScript.rightTorso_Health <= 0) {
+				RightTorsoHealth.Find ("RightTorsoRed").gameObject.SetActive (false);
+			} 
+			else if (playerStatScript.rightTorso_Health <= 30) {
+				RightTorsoHealth.Find ("RightTorsoGreen").gameObject.SetActive (false);
+				RightTorsoHealth.Find ("RightTorsoOrange").gameObject.SetActive (true);
+			}
+			else if (playerStatScript.rightTorso_Health <= 15) {
+				RightTorsoHealth.Find ("RightTorsoOrange").gameObject.SetActive (false);
+				RightTorsoHealth.Find ("RightTorsoRed").gameObject.SetActive (true);
+			}
+			//Left Weapon
+			if (playerStatScript.leftWeaponSystem_Health <= 0) {
+				LeftArmHealth.Find ("LeftArmRed").gameObject.SetActive (false);
+			} 
+			else if (playerStatScript.leftWeaponSystem_Health <= 15) {
+				LeftArmHealth.Find ("LeftArmGreen").gameObject.SetActive (false);
+				LeftArmHealth.Find ("LeftArmOrange").gameObject.SetActive (true);
+			}
+			else if (playerStatScript.leftWeaponSystem_Health <= 10) {
+				LeftArmHealth.Find ("LeftArmOrange").gameObject.SetActive (false);
+				LeftArmHealth.Find ("LeftArmRed").gameObject.SetActive (true);
+			}
+			//Right Weapon
+			if (playerStatScript.rightWeaponSystem_Health <= 0) {
+				RightArmHealth.Find ("RightArmRed").gameObject.SetActive (false);
+			} 
+			else if (playerStatScript.rightWeaponSystem_Health <= 15) {
+				RightArmHealth.Find ("RightArmGreen").gameObject.SetActive (false);
+				RightArmHealth.Find ("RightArmOrange").gameObject.SetActive (true);
+			}
+			else if (playerStatScript.rightWeaponSystem_Health <= 10) {
+				RightArmHealth.Find ("RightArmOrange").gameObject.SetActive (false);
+				RightArmHealth.Find ("RightArmRed").gameObject.SetActive (true);
+			}
+			//Left Leg
+			if (playerStatScript.leftLeg_Health <= 0) {
+				LeftLegHealth.Find ("LeftLegRed").gameObject.SetActive (false);
+			} 
+			else if (playerStatScript.leftLeg_Health <= 20) {
+				LeftLegHealth.Find ("RightArmGreen").gameObject.SetActive (false);
+				LeftLegHealth.Find ("LeftLegOrange").gameObject.SetActive (true);
+			}
+			else if (playerStatScript.leftLeg_Health <= 10) {
+				LeftLegHealth.Find ("LeftLegOrange").gameObject.SetActive (false);
+				LeftLegHealth.Find ("LeftLegRed").gameObject.SetActive (true);
+			}
+			//Right Leg
+			if (playerStatScript.rightTorso_Health <= 0) {
+				RightLegHealth.Find ("RightLegRed").gameObject.SetActive (false);
+			} 
+			else if (playerStatScript.rightTorso_Health <= 20) {
+				RightLegHealth.Find ("RightLegGreen").gameObject.SetActive (false);
+				RightLegHealth.Find ("RightLegOrange").gameObject.SetActive (true);
+			}
+			else if (playerStatScript.rightTorso_Health <= 10) {
+				RightLegHealth.Find ("RightLegOrange").gameObject.SetActive (false);
+				RightLegHealth.Find ("RightLegRed").gameObject.SetActive (true);
+			}
+		}
 	}
 }
